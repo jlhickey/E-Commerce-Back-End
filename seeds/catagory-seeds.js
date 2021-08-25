87028615357d55ce6b9ed45ce7563138706f4b1e
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-08-25 19:53:45 -0400

    Update catagory-seeds.js

--- seeds/catagory-seeds.js
+++ seeds/catagory-seeds.js
@@ -1,1 +1,24 @@
+const { Category } = require('../models');
 
+//Seed data for the Category table
+const categoryData = [
+  {
+    category_name: 'television',
+  },
+  {
+    category_name: 'pictures',
+  },
+  {
+    category_name: 'pencils',
+  },
+  {
+    category_name: 'sweatshirt',
+  },
+  {
+    category_name: 'Shoes',
+  },
+];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
