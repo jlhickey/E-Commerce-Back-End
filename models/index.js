14cfccf4125a02c91260bc495ac3c89665fd5af5
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-08-24 20:24:38 -0400

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,14 @@
+// import models
+const User = require('./User');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
