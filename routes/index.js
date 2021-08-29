a2f3ac840c9b3f10c554d42e17af1b8a952f9413
jlhickey <82125052+jlhickey@users.noreply.github.com>
2021-08-29 13:12:29 -0400

    updated coding

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+//Require express and the different express routes that are defined
 const router = require('express').Router();
-const apiRouter = require('/api');
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
 
-router.use('/api, apiRouters);
-           
-router.use((req, res) =>{
-  res.send(wrong route")
-});
+//Set the routes for express to use
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
